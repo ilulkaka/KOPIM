4d7d5aa180949bc139fa3e9dcb5e821012b313cd
ilulkaka <ilul.kaka@gmail.com>
2022-11-22 09:45:41 +0700

    judul main

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/assets/img/NPMI_Logo.png') }}">
-    <title>PT. NPR Mfg Ind</title>
+    <title>KOPIM</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Font Awesome -->
