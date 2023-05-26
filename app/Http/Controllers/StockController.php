2e08ce60124b1762ab9ad72705ce45744f77b11e
ilulkaka <ilul.kaka.studi@gmail.com>
2023-05-26 09:06:01 +0700

    stock brg

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -24,43 +24,43 @@ class StockController extends Controller
         $start = (int) $request->input('start');
         $length = (int) $request->input('length');
 
-        // $Datas = DB::select(
-        //     "SELECT a.kode, a.nama, a.spesifikasi, a.supplier, COALESCE(b.qty_in,0)as qty_in, COALESCE(c.qty_out,0)as qty_out, coalesce((b.qty_in) - ifnull(c.qty_out,0),0) as stock FROM
-        //     (SELECT * FROM tb_barang)a
-        //     LEFT JOIN
-        //     (SELECT kode, sum(qty_in)as qty_in FROM tb_in GROUP BY kode)b on b.kode=a.kode
-        //     LEFT JOIN
-        //     (SELECT kode, sum(qty_out)as qty_out FROM tb_out GROUP BY kode)c on c.kode=a.kode
-        //     where (a.kode like '%$search%' or a.nama like '%$search%')
-        //     LIMIT  $length OFFSET $start  "
-        // );
+        $Datas = DB::select(
+            "SELECT a.kode, a.nama, a.spesifikasi, a.supplier, COALESCE(b.qty_in,0)as qty_in, COALESCE(c.qty_out,0)as qty_out, coalesce((b.qty_in) - ifnull(c.qty_out,0),0) as stock FROM
+            (SELECT * FROM tb_barang)a
+            LEFT JOIN
+            (SELECT kode, sum(qty_in)as qty_in FROM tb_in GROUP BY kode)b on b.kode=a.kode
+            LEFT JOIN
+            (SELECT kode, sum(qty_out)as qty_out FROM tb_out GROUP BY kode)c on c.kode=a.kode
+            where (a.kode like '%$search%' or a.nama like '%$search%')
+            LIMIT  $length OFFSET $start  "
+        );
 
-        $Datas = DB::table('tb_barang as a')
-            ->leftJoin('tb_in as b', 'b.kode', '=', 'a.kode')
-            ->leftJoin('tb_out as c', 'c.kode', '=', 'a.kode')
-            ->select(
-                DB::raw(
-                    'a.kode, a.nama, a.spesifikasi, a.supplier, sum(coalesce(b.qty_in,0))as qty_in, sum(coalesce(c.qty_out,0))as qty_out, (sum(coalesce(b.qty_in,0)) - sum(coalesce(c.qty_out,0))) as stock '
-                )
-            )
-            ->where(function ($q) use ($search) {
-                $q
-                    ->where('a.kode', 'like', '%' . $search . '%')
-                    ->orwhere('a.nama', 'like', '%' . $search . '%')
-                    ->orwhere('a.spesifikasi', 'like', '%' . $search . '%')
-                    ->orwhere('a.supplier', 'like', '%' . $search . '%');
-            })
-            ->groupBy(
-                'a.kode',
-                'a.nama',
-                'a.spesifikasi',
-                'a.supplier',
-                'b.kode',
-                'c.kode'
-            )
-            ->skip($start)
-            ->take($length)
-            ->get();
+        // $Datas = DB::table('tb_barang as a')
+        //     ->leftJoin('tb_in as b', 'b.kode', '=', 'a.kode')
+        //     ->leftJoin('tb_out as c', 'c.kode', '=', 'a.kode')
+        //     ->select(
+        //         DB::raw(
+        //             'a.kode, a.nama, a.spesifikasi, a.supplier, sum(coalesce(b.qty_in,0))as qty_in, sum(coalesce(c.qty_out,0))as qty_out, (sum(coalesce(b.qty_in,0)) - sum(coalesce(c.qty_out,0))) as stock '
+        //         )
+        //     )
+        //     ->where(function ($q) use ($search) {
+        //         $q
+        //             ->where('a.kode', 'like', '%' . $search . '%')
+        //             ->orwhere('a.nama', 'like', '%' . $search . '%')
+        //             ->orwhere('a.spesifikasi', 'like', '%' . $search . '%')
+        //             ->orwhere('a.supplier', 'like', '%' . $search . '%');
+        //     })
+        //     ->groupBy(
+        //         'a.kode',
+        //         'a.nama',
+        //         'a.spesifikasi',
+        //         'a.supplier',
+        //         'b.kode',
+        //         'c.kode'
+        //     )
+        //     ->skip($start)
+        //     ->take($length)
+        //     ->get();
 
         $count = DB::table('tb_barang as a')
             ->leftJoin('tb_in as b', 'b.kode', '=', 'a.kode')
