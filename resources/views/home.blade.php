75ec1fcda7c41016669a82588fafbcf8f43e4b4f
ilulkaka <ilul.kaka.studi@gmail.com>
2024-03-13 12:58:47 +0700

    qr center

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -128,10 +128,9 @@
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-body">
-                    <div class="row" style="margin-top: -1%;">
+                    <div class="row justify-content-center" style="margin-top: -1%;">
                         {!! $qrCode !!}
                     </div>
-
                     <h5 style="text-align: center;"><b>{{ Auth::user()->name }} </b></h5>
                     <h5 style="text-align: center;">{{ $no_barcode }}</h5>
                 </div>
